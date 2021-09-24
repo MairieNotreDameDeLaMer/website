f41baea340940e28c5da7a4174732452211c1f09
Guillaume Rahbari <guillaume.rahbari@protonmail.com>
2021-09-24 14:21:16 +0200

    feat: improve header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,13 @@
-import { Link } from 'gatsby'
+import { Link as RouterLink } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import Box from '@mui/material/Box'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import SvgIcon from '@mui/material/SvgIcon'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Link from '@mui/material/Link'
 import Logo from '../images/logo.svg'
 
 export default function Layout({ pageTitle, children }) {
@@ -16,30 +19,38 @@ export default function Layout({ pageTitle, children }) {
       <header>
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6">NOTRE-DAME-DE-LA-MER</Typography>
-              <SvgIcon
-                component={Logo}
-                viewBox="0 0 800 500"
-                style={{ fontSize: '13rem' }}
-              />
-              <Typography variant="h6">
-                Canton de Bonnières-sur-Seine 78
-              </Typography>
+            <Toolbar sx={{ justifyContent: 'space-between' }}>
+              <Box sx={{ width: 300, height: 150 }}>
+                <Logo viewBox="0 0 800 508" width="100%" height="100%" />
+              </Box>
+              <nav>
+                <List sx={{ display: 'inline-flex' }}>
+                  <ListItem>
+                    <Link
+                      component={RouterLink}
+                      color="secondary"
+                      underline="hover"
+                      to="/"
+                    >
+                      Accueil
+                    </Link>
+                  </ListItem>
+                  <ListItem>
+                    <Link
+                      component={RouterLink}
+                      color="secondary"
+                      underline="hover"
+                      to="/about"
+                    >
+                      About
+                    </Link>
+                  </ListItem>
+                </List>
+              </nav>
             </Toolbar>
           </AppBar>
         </Box>
       </header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
       <main>{children}</main>
     </div>
   )
